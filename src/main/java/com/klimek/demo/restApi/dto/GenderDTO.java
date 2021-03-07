7a7b2ad86bba7908c6f7f61af957d9a25a9d7205
KomputerMisia <klimekd1@o2.pl>
2021-03-07 13:35:34 +0100

    Add implementation of dto token and dto gender.

--- src/main/java/com/klimek/demo/restApi/dto/GenderDTO.java
+++ src/main/java/com/klimek/demo/restApi/dto/GenderDTO.java
@@ -1,4 +1,18 @@
 package com.klimek.demo.restApi.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
+@AllArgsConstructor
+@Getter
 public class GenderDTO {
+
+    private String variant;
+    private String inputData;
+    private String gender;
+
 }
--- src/main/java/com/klimek/demo/restApi/dto/TokenDTO.java
+++ src/main/java/com/klimek/demo/restApi/dto/TokenDTO.java
@@ -1,4 +1,19 @@
 package com.klimek.demo.restApi.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
+@AllArgsConstructor
+@Getter
 public class TokenDTO {
+
+    private Long id;
+    private String name;
+    private String gender;
+
+
 }
