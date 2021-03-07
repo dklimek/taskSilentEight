67ba182bf77b75418eb183f2c703b07cb3a2d88b
KomputerMisia <klimekd1@o2.pl>
2021-03-07 13:34:23 +0100

    Add implementation of token entity.

--- src/main/java/com/klimek/demo/restApi/entities/Token.java
+++ src/main/java/com/klimek/demo/restApi/entities/Token.java
@@ -1,4 +1,27 @@
 package com.klimek.demo.restApi.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "tokens")
 public class Token {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String gender;
+
+    public Token(String name, String gender) {
+        this.name = name;
+        this.gender = gender;
+    }
+
 }
