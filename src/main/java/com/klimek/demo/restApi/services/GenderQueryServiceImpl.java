c64a69b0a01270b195aac61293f5bb7fd63b4196
KomputerMisia <klimekd1@o2.pl>
2021-03-07 13:40:41 +0100

    Add missing method in gender service.

--- src/main/java/com/klimek/demo/restApi/services/GenderQueryServiceImpl.java
+++ src/main/java/com/klimek/demo/restApi/services/GenderQueryServiceImpl.java
@@ -1,8 +1,36 @@
 package com.klimek.demo.restApi.services;
 
-public class GenderQueryServiceImpl implements GenderQueryService{
+import com.klimek.demo.restApi.dto.GenderDTO;
+import com.klimek.demo.restApi.dto.TokenDTO;
+import com.klimek.demo.restApi.repositories.TokenRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-    public GenderQueryServiceImpl(TokenRepository repository) {
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class GenderQueryServiceImpl implements GenderQueryService {
+
+    @Autowired
+    private TokenRepository tokenRepository;
+
+    public GenderQueryServiceImpl(TokenRepository tokenRepository) {
+        this.tokenRepository = tokenRepository;
+    }
+
+    @Override
+    public List<TokenDTO> getAllTokenList() {
+        List<TokenDTO> genderList = new ArrayList<>();
+
+        return genderList;
+    }
+
+    @Override
+    public GenderDTO getGenderByVariantAndInputData(String variant, String inputData) {
+
+        return new GenderDTO();
 
     }
+
 }
