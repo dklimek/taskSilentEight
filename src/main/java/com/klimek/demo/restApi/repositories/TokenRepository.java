b3529f20b742ea02be3de8ebdf11354fb54781b6
KomputerMisia <klimekd1@o2.pl>
2021-03-07 13:36:21 +0100

    Add implementation of jpa repository interface.

--- src/main/java/com/klimek/demo/restApi/repositories/TokenRepository.java
+++ src/main/java/com/klimek/demo/restApi/repositories/TokenRepository.java
@@ -1,4 +1,12 @@
 package com.klimek.demo.restApi.repositories;
 
-public interface TokenRepository {
+import com.klimek.demo.restApi.entities.Token;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.Optional;
+
+@RepositoryRestResource(collectionResourceRel = "tokens", path = "tokens")
+public interface TokenRepository extends CrudRepository<Token, Long> {
+    Optional<Token> findTopByName(String name);
 }
