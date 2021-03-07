f805dd92a7282e378baddb9145010f230cda94f0
KomputerMisia <klimekd1@o2.pl>
2021-03-07 13:38:17 +0100

    Add implementation of gender service interface.

--- src/main/java/com/klimek/demo/restApi/services/GenderQueryService.java
+++ src/main/java/com/klimek/demo/restApi/services/GenderQueryService.java
@@ -1,4 +1,15 @@
 package com.klimek.demo.restApi.services;
 
+import com.klimek.demo.restApi.dto.GenderDTO;
+import com.klimek.demo.restApi.dto.TokenDTO;
+
+import java.util.List;
+
 public interface GenderQueryService {
+
+    List<TokenDTO> getAllTokenList();
+
+    GenderDTO getGenderByVariantAndInputData(String variant, String inputData);
+
 }
+
