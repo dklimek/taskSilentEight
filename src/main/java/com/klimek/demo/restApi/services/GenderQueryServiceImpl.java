bf952f7a649f55f5f7d47fd64ff8ae70028fb35c
KomputerMisia <klimekd1@o2.pl>
2021-03-07 13:47:38 +0100

    Add implementation of method in gender service.

--- src/main/java/com/klimek/demo/restApi/services/GenderQueryServiceImpl.java
+++ src/main/java/com/klimek/demo/restApi/services/GenderQueryServiceImpl.java
@@ -23,13 +23,39 @@ public class GenderQueryServiceImpl implements GenderQueryService {
     public List<TokenDTO> getAllTokenList() {
         List<TokenDTO> genderList = new ArrayList<>();
 
+        tokenRepository.findAll().forEach(token -> {
+            genderList.add(new TokenDTO(token.getId(), token.getName(), token.getGender()));
+        });
+
         return genderList;
     }
 
     @Override
     public GenderDTO getGenderByVariantAndInputData(String variant, String inputData) {
 
-        return new GenderDTO();
+        String[] tokenList = inputData.trim().split(" ");
+
+        if (variant.equals("token")) {
+            if (tokenRepository.findTopByName(tokenList[0]).isPresent()) {
+                return new GenderDTO(variant, inputData, tokenRepository.findTopByName(tokenList[0]).get().getGender());
+            } else {
+                throw new TokenNotfoundException();
+            }
+        } else if (variant.equals("string")) {
+            int result = 0;
+            for (String token : tokenList) {
+
+                if (tokenRepository.findTopByName(token).isPresent()) {
+                    String tokenGender = tokenRepository.findTopByName(token).get().getGender();
+                    result += (tokenGender.contains("M") ? 1 : -1);
+                }
+            }
+            String gender = result > 0 ? "Male" : result < 0 ? "Female" : "INCONCLUSIVE";
+            return new GenderDTO(variant, inputData, gender);
+        } else {
+            throw new VariantBadRequestException();
+        }
+
 
     }
 
